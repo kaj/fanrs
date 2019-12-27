97427a5544960599e5143c78953227d900cf4eb9
Rasmus Kaj <kaj@kth.se>
2019-12-27 16:53:21 +0100

    Some more cleanup of count-pages.

--- src/count_pages.rs
+++ src/count_pages.rs
@@ -13,15 +13,15 @@ pub struct CountPages {
 impl CountPages {
     pub fn run(&self) -> Result<(), Error> {
         let mut issue = self.issue.clone();
-        let mut page = self.pages.iter();
-        let mut prev = page.next().ok_or(format_err!("Too few arguments"))?;
-        while let Some(page) = page.next() {
-            if page <= prev {
-                return Err(format_err!("Page numbers must increase"));
+        for pages in self.pages.windows(2) {
+            // Irrefutable, because pages are 2-windows of a slice.
+            if let [start, end] = pages {
+                if end <= start {
+                    return Err(format_err!("Page numbers must increase"));
+                }
+                println!("{:>6}: {:3}", issue, end - start);
+                issue = (issue.last() + 1).to_string().parse()?;
             }
-            println!("{:>6}: {:3}", issue, page - prev);
-            prev = page;
-            issue = (issue.last() + 1).to_string().parse()?;
         }
         Ok(())
     }
