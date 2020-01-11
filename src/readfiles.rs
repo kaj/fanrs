0fa3189f13f33bda2d069285bec279c67b19f1bd
Rasmus Kaj <kaj@kth.se>
2020-01-11 19:30:03 +0100

    Switch to roxmltree.
    Possibly a cleaner / more efficient xml parsing library.

--- src/readfiles.rs
+++ src/readfiles.rs
@@ -6,12 +6,14 @@ use chrono::{Datelike, Local, NaiveDate};
 use diesel::prelude::*;
 use failure::{format_err, Error};
 use io_result_optional::IoResultOptional;
-use std::fs::File;
+use roxmltree::{Document, Node};
+use slug::slugify;
+use std::fs::read_to_string;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
-use xmltree::Element;
 
 type Result<T> = std::result::Result<T, Error>;
+static XMLNS: &str = "http://www.w3.org/XML/1998/namespace";
 
 #[derive(StructOpt)]
 pub struct Args {
@@ -61,29 +63,20 @@ fn load_year(base: &Path, year: i16, db: &PgConnection) -> Result<()> {
 }
 
 fn do_load_year(base: &Path, year: i16, db: &PgConnection) -> Result<()> {
-    if let Some(file) =
-        File::open(base.join(format!("{}.data", year))).optional()?
+    if let Some(data) =
+        read_to_string(base.join(format!("{}.data", year))).optional()?
     {
-        for i in Element::parse(file)?.children {
-            match i.name.as_ref() {
+        for elem in child_elems(Document::parse(&data)?.root_element()) {
+            match elem.tag_name().name() {
                 "info" => (), // ignore
-                "issue" => register_issue(year, &i, db).map_err(|e| {
+                "issue" => register_issue(year, &elem, db).map_err(|e| {
                     format_err!(
                         "In issue {}: {}",
-                        i.attributes
-                            .get("nr")
-                            .map(|n| n.as_ref())
-                            .unwrap_or("?"),
+                        elem.attribute("nr").unwrap_or("?"),
                         e
                     )
                 })?,
-                other => {
-                    return Err(format_err!(
-                        "Unexpected element {:?} in year {}",
-                        other,
-                        year,
-                    ))
-                }
+                _other => return Err(format_err!("Unexpected {:?}", elem)),
             }
         }
     } else {
@@ -92,29 +85,27 @@ fn do_load_year(base: &Path, year: i16, db: &PgConnection) -> Result<()> {
     Ok(())
 }
 
-fn register_issue(year: i16, i: &Element, db: &PgConnection) -> Result<()> {
+fn register_issue(year: i16, i: &Node, db: &PgConnection) -> Result<()> {
     let nr = i
-        .attributes
-        .get("nr")
+        .attribute("nr")
         .ok_or_else(|| format_err!("nr missing"))
         .and_then(|s| Ok(s.parse()?))?;
     let issue = Issue::get_or_create(
         year,
         nr,
-        i.attributes.get("pages").and_then(|s| s.parse().ok()),
-        i.attributes.get("price").and_then(|s| s.parse().ok()),
-        i.get_child("omslag").and_then(get_best_plac),
+        i.attribute("pages").and_then(|s| s.parse().ok()),
+        i.attribute("price").and_then(|s| s.parse().ok()),
+        get_child(*i, "omslag").and_then(get_best_plac),
         db,
     )?;
     println!("Found issue {}", issue);
     issue.clear(db)?;
 
-    for (seqno, c) in i.children.iter().enumerate() {
-        match c.name.as_ref() {
+    for (seqno, c) in child_elems(*i).enumerate() {
+        match c.tag_name().name() {
             "omslag" => {
-                if let Some(by) = c.get_child("by") {
-                    let by = get_creators(by, db)?;
-                    for creator in by {
+                if let Some(by) = get_child(c, "by") {
+                    for creator in get_creators(by, db)? {
                         use crate::schema::covers_by::dsl as cb;
                         diesel::insert_into(cb::covers_by)
                             .values((
@@ -126,8 +117,8 @@ fn register_issue(year: i16, i: &Element, db: &PgConnection) -> Result<()> {
                     }
                 }
             }
-            "text" => register_article(&issue, seqno, &c, db)?,
-            "serie" => register_serie(&issue, seqno, &c, db)?,
+            "text" => register_article(&issue, seqno, c, db)?,
+            "serie" => register_serie(&issue, seqno, c, db)?,
             "skick" => (), // ignore
             _ => {
                 return Err(format_err!(
@@ -144,25 +135,21 @@ fn register_issue(year: i16, i: &Element, db: &PgConnection) -> Result<()> {
 fn register_article(
     issue: &Issue,
     seqno: usize,
-    c: &Element,
+    c: Node,
     db: &PgConnection,
 ) -> Result<()> {
     let article = Article::get_or_create(
         get_req_text(c, "title")?,
-        get_text_norm(c, "subtitle").as_ref().map(|s| s.as_ref()),
-        get_text_norm(c, "note").as_ref().map(|s| s.as_ref()),
+        get_text_norm(c, "subtitle").as_deref(),
+        get_text_norm(c, "note").as_deref(),
         db,
     )?;
     article.publish(issue.id, seqno as i16, db)?;
-    for e in &c.children {
-        match e.name.as_ref() {
+    for e in child_elems(c) {
+        match e.tag_name().name() {
             "title" | "subtitle" | "note" => (), // handled above
             "by" => {
-                let role = e
-                    .attributes
-                    .get("role")
-                    .map(|r| r.as_ref())
-                    .unwrap_or("by");
+                let role = e.attribute("role").unwrap_or("by");
                 for by in get_creators(e, db)? {
                     use crate::schema::articles_by::dsl as ab;
                     diesel::insert_into(ab::articles_by)
@@ -175,7 +162,7 @@ fn register_article(
                         .execute(db)?;
                 }
             }
-            "ref" => article.set_refs(&parse_refs(&e.children)?, db)?,
+            "ref" => article.set_refs(&parse_refs(e)?, db)?,
             _other => return Err(format_err!("Unknown {:?} in text", e)),
         }
     }
@@ -185,42 +172,40 @@ fn register_article(
 fn register_serie(
     issue: &Issue,
     seqno: usize,
-    c: &Element,
+    c: Node,
     db: &PgConnection,
 ) -> Result<()> {
     use crate::schema::episodes::dsl as e;
     use crate::schema::episodes_by::dsl as eb;
     let episode = Episode::get_or_create(
-        &Title::get_or_create(get_req_text(&c, "title")?, db)?,
-        get_text_norm(c, "episode").as_ref().map(|t| t.as_ref()),
-        get_text_norm(c, "teaser").as_ref().map(|t| t.as_ref()),
-        get_text_norm(c, "note").as_ref().map(|t| t.as_ref()),
-        get_text_norm(c, "copyright").as_ref().map(|t| t.as_ref()),
+        &Title::get_or_create(get_req_text(c, "title")?, db)?,
+        get_text_norm(c, "episode").as_deref(),
+        get_text_norm(c, "teaser").as_deref(),
+        get_text_norm(c, "note").as_deref(),
+        get_text_norm(c, "copyright").as_deref(),
         db,
     )?;
-    let part = c.get_child("part");
+    let part = get_child(c, "part");
     Part::publish(
         &episode,
-        part.and_then(|p| p.attributes.get("no"))
+        part.and_then(|p| p.attribute("no"))
             .and_then(|n| n.parse().ok()),
-        part.and_then(|p| p.text.as_ref()).map(|s| s.as_ref()),
+        part.and_then(|p| p.text()),
         &issue,
         Some(seqno as i16),
         get_best_plac(c),
         &get_text_norm(c, "label").unwrap_or_default(),
         db,
     )?;
-    for e in &c.children {
-        match e.name.as_ref() {
-            "episode"
-                if e.attributes.get("role") == Some(&"orig".to_string()) =>
-            {
-                let lang =
-                    e.attributes.get("lang").expect("orig should have lang");
+    for e in child_elems(c) {
+        match e.tag_name().name() {
+            "episode" if e.attribute("role") == Some("orig") => {
+                let lang = e
+                    .attribute((XMLNS, "lang"))
+                    .expect("orig should have lang");
                 let orig = e
-                    .text
-                    .as_ref()
-                    .map(|t| normalize_space(&t))
+                    .text()
+                    .map(normalize_space)
                     .expect("orig should have name");
                 diesel::update(e::episodes)
                     .set((e::orig_lang.eq(lang), e::orig_episode.eq(orig)))
@@ -230,12 +215,8 @@ fn register_serie(
             "label" | "title" | "episode" | "teaser" | "part" | "note"
             | "copyright" | "best" => (), // handled above
             "by" => {
-                let role = e
-                    .attributes
-                    .get("role")
-                    .map(|r| r.as_ref())
-                    .unwrap_or("by");
-                for by in get_creators(&e, db)? {
+                let role = e.attribute("role").unwrap_or("by");
+                for by in get_creators(e, db)? {
                     diesel::insert_into(eb::episodes_by)
                         .values((
                             eb::episode_id.eq(episode.id),
@@ -247,11 +228,14 @@ fn register_serie(
                 }
             }
             "ref" => {
-                episode.set_refs(&parse_refs(&e.children)?, db).map_err(
-                    |err| format_err!("{} while handling {:?}", err, e),
-                )?
+                episode.set_refs(&parse_refs(e)?, db).map_err(|err| {
+                    format_err!("{} while handling {:?}", err, e)
+                })?
             }
-            "prevpub" => match e.children.get(0).map(|e| e.name.as_ref()) {
+            "prevpub" => match e
+                .first_element_child()
+                .map(|e| e.tag_name().name())
+            {
                 Some("fa") => {
                     let nr = get_text(e, "fa").unwrap().parse()?;
                     let year = get_text(e, "year")
@@ -270,10 +254,7 @@ fn register_serie(
                         db,
                     )?;
                 }
-                Some("date")
-                    if e.attributes.get("role")
-                        == Some(&"orig".to_string()) =>
-                {
+                Some("date") if e.attribute("role") == Some("orig") => {
                     let date: NaiveDate =
                         get_text(e, "date").unwrap().parse()?;
                     diesel::update(e::episodes)
@@ -306,8 +287,7 @@ fn register_serie(
                 if let Some(from) = get_text(e, "from") {
                     let from: NaiveDate = from.parse()?;
                     let to: NaiveDate = get_req_text(e, "to")?.parse()?;
-                    let sun =
-                        e.attributes.get("d") == Some(&"sun".to_string());
+                    let sun = e.attribute("d") == Some("sun");
                     diesel::update(e::episodes)
                         .set((
                             e::orig_date.eq(Some(from)),
@@ -336,88 +316,103 @@ fn register_serie(
     Ok(())
 }
 
-fn parse_refs(refs: &[Element]) -> Result<Vec<RefKey>> {
-    refs.iter().map(parse_ref).collect()
+fn parse_refs(parent: Node) -> Result<Vec<RefKey>> {
+    child_elems(parent).map(parse_ref).collect()
 }
 
-fn parse_ref(e: &Element) -> Result<RefKey> {
-    match e.name.as_ref() {
+fn parse_ref(e: Node) -> Result<RefKey> {
+    match e.tag_name().name() {
         "fa" => e
-            .attributes
-            .get("no")
-            .map(|s| RefKey::fa(s))
+            .attribute("no")
+            .map(RefKey::fa)
             .ok_or_else(|| format_err!("Fa witout no: {:?}", e)),
         "key" => e
-            .text
-            .as_ref()
-            .map(|s| RefKey::key(s))
+            .text()
+            .map(RefKey::key)
             .ok_or_else(|| format_err!("Key without text: {:?}", e)),
         "who" => e
-            .text
-            .as_ref()
-            .map(|s| RefKey::who(s))
+            .text()
+            .map(RefKey::who)
             .ok_or_else(|| format_err!("Who without name: {:?}", e)),
         "serie" => e
-            .text
-            .as_ref()
-            .map(|s| RefKey::title(s))
+            .text()
+            .map(RefKey::title)
             .ok_or_else(|| format_err!("Serie without title: {:?}", e)),
-        _ => Err(format_err!("Unknown refernce: {:?}", e)),
+        _ => Err(format_err!("Unknown reference: {:?}", e)),
     }
 }
 
-fn get_req_text<'a>(e: &'a Element, name: &str) -> Result<&'a str> {
+#[test]
+fn test_parse_refs() -> Result<()> {
+    let doc = "<ref>
+        <fa no=\"12\"/>
+        <key>Marion Trelawny</key>
+        <who>Jaime Vallvé</who>
+        <serie>Johan Vilde</serie>
+      </ref>\n";
+
+    assert_eq!(
+        parse_refs(Document::parse(doc)?.root_element())?,
+        vec![
+            RefKey::fa("12"),
+            RefKey::key("Marion Trelawny"),
+            RefKey::who("Jaime Vallvé"),
+            RefKey::title("Johan Vilde"),
+        ],
+    );
+    Ok(())
+}
+
+fn get_req_text<'a>(e: Node<'a, 'a>, name: &str) -> Result<&'a str> {
     get_text(e, name)
         .ok_or_else(|| format_err!("{:?} missing child {}", e, name))
 }
 
-fn get_text<'a>(e: &'a Element, name: &str) -> Option<&'a str> {
-    e.get_child(name)
-        .and_then(|e| e.text.as_ref().map(|s| s.as_ref()))
-}
-fn get_text_norm<'a>(e: &'a Element, name: &str) -> Option<String> {
-    e.get_child(name)
-        .and_then(|e| e.text.as_ref().map(|s| normalize_space(&s)))
+fn get_text<'a>(e: Node<'a, 'a>, name: &str) -> Option<&'a str> {
+    get_child(e, name).and_then(|e| e.text())
 }
 
-fn get_best_plac(e: &Element) -> Option<i16> {
-    e.get_child("best")
-        .and_then(|e| e.attributes.get("plac").and_then(|s| s.parse().ok()))
+fn get_text_norm(e: Node, name: &str) -> Option<String> {
+    get_text(e, name).map(normalize_space)
 }
 
-fn get_creators(by: &Element, db: &PgConnection) -> Result<Vec<Creator>> {
-    let one_creator = |e: &Element| {
-        let name = &e
-            .text
-            .as_ref()
+fn get_best_plac(e: Node) -> Option<i16> {
+    get_child(e, "best")
+        .and_then(|e| e.attribute("plac").and_then(|s| s.parse().ok()))
+}
+
+fn get_creators(by: Node, db: &PgConnection) -> Result<Vec<Creator>> {
+    let one_creator = |e: Node| -> Result<Creator> {
+        let name = e
+            .text()
             .ok_or_else(|| format_err!("missing name in {:?}", e))?;
         Ok(Creator::get_or_create(name, db).map_err(|e| {
             format_err!("Failed to create creator {:?}: {}", name, e)
         })?)
     };
-    let c = &by.children;
-    if c.is_empty() {
+    let who = child_elems(by)
+        .map(one_creator)
+        .collect::<Result<Vec<_>>>()?;
+    if who.is_empty() {
         // No child elements, a single name directly in the by element
         Ok(vec![one_creator(by)?])
     } else {
         // Child <who> elements, each containing a name.
-        c.iter().map(one_creator).collect()
+        Ok(who)
     }
 }
 
 fn read_persondata(base: &Path, db: &PgConnection) -> Result<()> {
     use crate::schema::creator_aliases::dsl as ca;
     use crate::schema::creators::dsl as c;
-    use slug::slugify;
-    let file = File::open(base.join("extra-people.data"))?;
-    for e in Element::parse(file)?.children {
-        match e.name.as_ref() {
+    let buf = read_to_string(base.join("extra-people.data"))?;
+    for e in child_elems(Document::parse(&buf)?.root_element()) {
+        match e.tag_name().name() {
             "person" => {
-                let name = get_req_text(&e, "name")?;
+                let name = get_req_text(e, "name")?;
                 let slug = e
-                    .attributes
-                    .get("slug")
-                    .cloned()
+                    .attribute("slug")
+                    .map(String::from)
                     .unwrap_or_else(|| slugify(&name));
                 let creator = c::creators
                     .select((c::id, c::name, c::slug))
@@ -442,12 +437,14 @@ fn read_persondata(base: &Path, db: &PgConnection) -> Result<()> {
                             })
                     })?;
 
-                for a in e.children.iter().filter(|a| a.name == "alias") {
-                    if let Some(ref alias) = a.text {
+                for a in
+                    e.children().filter(|a| a.tag_name().name() == "alias")
+                {
+                    if let Some(alias) = a.text() {
                         ca::creator_aliases
                             .select(ca::id)
                             .filter(ca::creator_id.eq(creator.id))
-                            .filter(ca::name.eq(&alias))
+                            .filter(ca::name.eq(alias))
                             .first::<i32>(db)
                             .optional()?
                             .ok_or(0)
@@ -568,6 +565,16 @@ fn delete_unpublished(db: &PgConnection) -> Result<()> {
         published_articles.len()
     );
     Ok(())
+}
+
+fn get_child<'a>(node: Node<'a, 'a>, name: &str) -> Option<Node<'a, 'a>> {
+    node.children().find(|a| a.tag_name().name() == name)
+}
+
+fn child_elems<'a, 'b>(
+    node: Node<'a, 'b>,
+) -> impl Iterator<Item = Node<'a, 'b>> {
+    node.children().filter(|n| n.is_element())
 }
 
 fn normalize_space(s: &str) -> String {
